059df7761ac22d5c0f625d7aa82a2261752bb104
Robert Adams <r0b4dams@gmail.com>
2024-02-29 12:15:38 -0800

    Add Food and gameover condition

--- src/lib/Game.ts
+++ src/lib/Game.ts
@@ -8,49 +8,82 @@ export default class Game {
   private ctlr: InputController;
   private board: Board;
   private snake: Snake;
+  private food: Square;
   private prevTime = 0;
+  private frameId: number | null = null;
 
-  constructor(public speed: number = 2) {
+  constructor(public speed: number = 1) {
     this.ctlr = new InputController();
     this.board = new Board("main");
     this.snake = new Snake(this.board.center);
-    this.render();
-    requestAnimationFrame(this.main);
+    this.food = new Square("food", this.board, this.board.randomGridPosition());
+    this.frameId = requestAnimationFrame(this.main);
   }
 
   @autobind
   main(currTime: DOMHighResTimeStamp) {
-    requestAnimationFrame(this.main);
+    this.frameId = requestAnimationFrame(this.main);
+
     const secondsSinceLastRender = (currTime - this.prevTime) / 1000;
     if (secondsSinceLastRender < 1 / this.speed) {
       return;
     }
+
     this.prevTime = currTime;
+
     this.update();
-    this.render();
+    if (this.frameId) {
+      this.render();
+    }
   }
 
   update() {
-    const next = this.snake.peek(this.ctlr.direction);
+    const { element, coordinates } = this.snake.check(this.ctlr.direction);
 
-    if (
-      next.x <= 0 ||
-      next.x > this.board.attributes["data-size"] ||
-      next.y <= 0 ||
-      next.y > this.board.attributes["data-size"]
-    ) {
-      alert("game over - out of bounds");
-      window.location.reload();
+    if (this.board.outsideGrid(coordinates)) {
+      this.gameover();
+      return;
     }
 
+    switch (element?.getAttribute("data-type")) {
+      case "snake":
+        this.gameover();
+        break;
+
+      case "food":
+        this.food.remove();
+        this.snake.grow();
+        let position, occupied;
+        do {
+          position = this.board.randomGridPosition();
+          occupied = this.board.square(position);
+        } while (occupied);
+        this.food = new Square("food", this.board, position);
+        break;
+    }
     this.snake.move(this.ctlr.direction);
   }
 
   render() {
     this.board.clear();
 
-    this.snake.body.forEach((s) => {
-      new Square("snake", this.board, { x: s.x, y: s.y });
+    this.snake.body.forEach(({ x, y }) => {
+      new Square("snake", this.board, { x, y });
     });
+
+    this.board.element.append(this.food.element);
+  }
+
+  victory() {
+    return this.snake.body.length === this.board.attributes["data-size"] ** 2;
+  }
+
+  gameover() {
+    console.log("gameover");
+
+    if (this.frameId) {
+      cancelAnimationFrame(this.frameId);
+      this.frameId = null;
+    }
   }
 }
